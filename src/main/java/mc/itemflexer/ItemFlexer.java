02ab56c5708b8467b992585a732794497784b8f1
P03W <pweidman.mail@gmail.com>
2020-11-22 12:30:12 -0500

    Add icon, optional slot argument

--- src/main/java/mc/itemflexer/ItemFlexer.java
+++ src/main/java/mc/itemflexer/ItemFlexer.java
@@ -1,35 +1,51 @@
 package mc.itemflexer;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.server.PlayerStream;
-import net.minecraft.entity.Entity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.text.*;
+import net.minecraft.text.LiteralText;
 import net.minecraft.util.Formatting;
-
-import java.time.format.TextStyle;
 
 public class ItemFlexer implements ModInitializer {
     @Override
     public void onInitialize() {
         System.out.println("Flex your items!");
         CommandRegistrationCallback.EVENT.register((dispatcher, dedicated) -> {
-            dispatcher.register(CommandManager.literal("flex").executes(context -> {
-                Entity entity = context.getSource().getEntityOrThrow();
-                if (entity instanceof ServerPlayerEntity) {
-                    ItemStack stack = ((ServerPlayerEntity)entity).getMainHandStack();
-                    if (stack.getItem() != Items.AIR) {
-                        for (Object player : PlayerStream.all(((ServerPlayerEntity)entity).server).toArray()) {
-                            ((ServerPlayerEntity)player).sendMessage(((LiteralText)entity.getDisplayName()).append(new LiteralText(" is flexing their ").formatted(Formatting.WHITE)).append(stack.toHoverableText()), false);
-                        }
-                    }
-                }
-                return 1;
-            }));
-        });
+                dispatcher.register(
+                    CommandManager.literal("flex")
+                        .executes(context -> {
+                            ServerPlayerEntity entity = context.getSource().getPlayer();
+                            ItemStack stack = entity.getMainHandStack();
+                            if (stack.getItem() != Items.AIR) {
+                                for (Object player : PlayerStream.all(entity.server).toArray()) {
+                                    ((ServerPlayerEntity)player).sendMessage(((LiteralText)entity.getDisplayName()).append(new LiteralText(" is flexing their ").formatted(Formatting.WHITE)).append(stack.toHoverableText()), false);
+                                }
+                            }
+                            return 1;
+                        })
+                        .then(CommandManager.argument("slot", IntegerArgumentType.integer(1, 9))
+                            .executes(context -> {
+                                System.out.println(context);
+                                int slotIndex = IntegerArgumentType.getInteger(context, "slot");
+                                ServerPlayerEntity entity = context.getSource().getPlayer();
+                                ItemStack stack = entity.inventory.getStack(slotIndex - 1);
+                                System.out.println(stack);
+                                System.out.println(entity);
+                                if (stack.getItem() != Items.AIR) {
+                                    for (Object player : PlayerStream.all(entity.server).toArray()) {
+                                        ((ServerPlayerEntity)player).sendMessage(((LiteralText)entity.getDisplayName()).append(new LiteralText(" is flexing their ").formatted(Formatting.WHITE)).append(stack.toHoverableText()), false);
+                                    }
+                                }
+                                return 1;
+                            })
+                        )
+                );
+            }
+        );
     }
 }
