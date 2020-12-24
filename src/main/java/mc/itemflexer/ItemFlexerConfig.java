d9e60d0902338f9a518e2ebcb0941db5550a507c
P03W <P03W@users.noreply.github.com>
2020-12-24 15:27:51 -0500

    Switch to microconfig, updated fabric api

--- src/main/java/mc/itemflexer/ItemFlexerConfig.java
+++ src/main/java/mc/itemflexer/ItemFlexerConfig.java
@@ -1,11 +1,9 @@
 package mc.itemflexer;
 
-import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
-import me.sargunvohra.mcmods.autoconfig1u.shadowed.blue.endless.jankson.Comment;
+import mc.microconfig.Comment;
+import mc.microconfig.ConfigData;
 
-@Config(name = "itemflexer")
 class ItemFlexerConfig implements ConfigData {
-    @Comment("Delay before /flex can be used again (in ticks)")
+    @Comment("Cooldown before /flex can be used again, in ticks")
     int cooldown = 0;
 }
