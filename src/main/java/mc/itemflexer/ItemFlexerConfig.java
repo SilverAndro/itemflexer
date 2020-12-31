8535ae37de67324a33b40576a9c42c9e765f3227
P03W <P03W@users.noreply.github.com>
2020-12-31 14:47:06 -0500

    Properly fix crash on launch

--- src/main/java/mc/itemflexer/ItemFlexerConfig.java
+++ src/main/java/mc/itemflexer/ItemFlexerConfig.java
@@ -3,7 +3,7 @@ package mc.itemflexer;
 import mc.microconfig.Comment;
 import mc.microconfig.ConfigData;
 
-class ItemFlexerConfig implements ConfigData {
+public class ItemFlexerConfig implements ConfigData {
     @Comment("Cooldown before /flex can be used again, in ticks")
     public int cooldown = 0;
 }
